f20012f8ca1282aaa5d866c2f02365547ba25359
AbdelrahmanSaeedHassan <abdelrahmansaeed1989@gmail.com>
2016-01-20 16:23:19 +0200

    Important ways

---
+++ ConsoleApplication21/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
